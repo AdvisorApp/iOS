39dcf3d636b10213d48165e59744fb92cee9c5a0
Clément Garbay <clementgarbay@gmail.com>
2016-05-27 22:44:09 +0200

    [ADV-29] Adding login view

---
+++ AdvisorApp/controller/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  AdvisorApp
+//
+//  Created by Clément GARBAY on 27/05/2016.
+//  Copyright © 2016 Clément GARBAY. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var identifierTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signin(sender: UIButton) {
+        // Check identifier and password
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
